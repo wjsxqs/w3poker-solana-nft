5aad353fbf3fd6348a440ea2961a617895facd7f
wjsxqs <xsmlcp@163.com>
2022-12-06 12:10:47 +0800

    Update test

--- test/buy.test.ts
+++ test/buy.test.ts
@@ -3,12 +3,14 @@ import { expect } from 'chai';
 import { buy } from '../src';
 import { connection, feePayer } from './helper';
 
-describe('buy', async () => {
-  it('buy to success', async () => {
-    const toAccount = Keypair.generate();
-
+describe('buy', () => {
+  beforeEach(async () => {
     const balance = await connection.getBalance(feePayer.publicKey);
     expect(balance).to.be.gt(0);
+  });
+
+  it('buy success', async () => {
+    const toAccount = Keypair.generate();
 
     const signature = await buy(connection, toAccount.publicKey, feePayer);
     console.log('signature', signature);
